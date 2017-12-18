b02d19190d74887aca6a3e09c86147a3d1dea0f6
Jan Montizaan <janmontizaan@gmail.com>
2017-12-18 14:35:53 +0100

    message tekst gewijzigd info

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -39,7 +39,7 @@ routes.get('/hello/*', function (req, res, next) {
 routes.get('/info', function (req, res, next) {
 	res.status(200);
 	res.json({
-		message: 'de volgende services zijn aanwezig:',
+		message: 'de onderstaande services zijn aanwezig:',
 		endpoint1: '/hello',
 		endpoint2: '/hello/error'
 	});
